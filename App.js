3fa902715300bd47a249817d064b6bc084be31bd
ranjeet <1705213032@ietlucknow.ac.in>
2020-08-28 03:24:19 +0530

    react native components part-1

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import Main from './components/MainComponent';
 
 export default class App extends React.Component {
-  render(){
+  render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app !</Text>
-        <Text>Changes you make will automatically reload</Text>
-        <Text>Shake your phone to open the developer menu</Text>
-      </View>
+      <Main />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: '#0ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
